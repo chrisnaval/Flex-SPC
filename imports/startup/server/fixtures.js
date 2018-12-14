aefca3d8ac2226c650f0e34f98944b755582ec47
Jemari Lapera <jemari.lapera@easehub.online>
2018-12-14 16:49:33 +0800

    Updates on Fixtures

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -4,43 +4,46 @@
 import { AppModules } from '/imports/api/collections/appModules/appModules.js';
 import { UserProfiles } from '/imports/api/collections/users/userProfiles.js';
 
-Meteor.startup(function () {
-  // Create Administrator on Users Collection
-  if(Meteor.users.find().count() === 0) {
-    var userProfileId = UserProfiles.insert({
-                        firstName: "Admin",
-                        lastName: "Administrator",
-                        address: "Cebu City",
-                        type: "admin",
-                        role: {}
-                      });
-    
-    var userProfile = UserProfiles.findOne(userProfileId);
+Meteor.startup(function() {
+    // Create Administrator on Users Collection
+    if(Meteor.users.find().count() === 0) {
+        var userProfileId = UserProfiles.insert({
+            firstName: "Admin",
+            lastName: "Administrator",
+            address: "Cebu City",
+            type: "admin",
+            role: {
+                _id: "",
+                role: ""
+            }
+        });
 
-    Accounts.createUser({
-      email: "admin@email.com",
-      password: "secret-passw0rt",
-      username: "admin",
-      profile: userProfile
-    });
-  }
-  
-  // Seed Data to AppModules Collection
-  if(AppModules.find().count() === 0) {
-    const modules = [
-      {
-        module: "Dashboard"
-      },
-      {
-        module: "Reports"
-      },
-      {
-        module: "Issue Tracker"
-      },
-    ];
+        var userProfile = UserProfiles.findOne(userProfileId);
 
-    modules.forEach(element => {
-      AppModules.insert(element);
-    });
-  }
+        Accounts.createUser({
+            email: "admin@email.com",
+            password: "secret-passw0rt",
+            username: "admin",
+            profile: userProfile
+        });
+    }
+
+    // Seed Data to AppModules Collection
+    if(AppModules.find().count() === 0) {
+        const modules = [
+            {
+                module: "Dashboard"
+            },
+            {
+                module: "Reports"
+            },
+            {
+                module: "Issue Tracker"
+            },
+        ];
+
+        modules.forEach(element => {
+            AppModules.insert(element);
+        });
+    }
 });
