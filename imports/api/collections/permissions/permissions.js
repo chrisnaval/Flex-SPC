3166ec08025ef07e5042a1231f99dac5c0bcb75c
Jemari Lapera <jemari.lapera@easehub.online>
2018-12-19 16:23:57 +0800

    Added Role and Permissions to Super Admin, Fixtures and Constants

--- imports/api/collections/permissions/permissions.js
+++ imports/api/collections/permissions/permissions.js
@@ -6,16 +6,16 @@ export const Permissions = new Mongo.Collection('permissions');
 
 // Schema
 Permissions.schema = new SimpleSchema({
-  module: {
-    type: String,
-    optional: false
-  },
-  function: {
-    type: String,
-    optional: false
-  },
-  permission: {
-    type: String,
-    optional: false
-  }
+    module: {
+        type: String,
+        optional: false
+    },
+    function: {
+        type: String,
+        optional: false
+    },
+    permission: {
+        type: String,
+        optional: false
+    }
 });
